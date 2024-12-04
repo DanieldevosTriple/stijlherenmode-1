0b62f69350b2b784fccf1a9fa3a2250b06be2945
Daniel de Vos <daniel.devos@wearetriple.com>
2024-12-04 17:54:02 +0100

    <script src="{{ 'quick-order-list.js' | asset_url }}" defer="defer"></script>

---
+++ assets/swatch-selection.js
@@ -0,0 +1,25 @@
+document.querySelectorAll('input[type="radio"][name="options"]').forEach(function(swatch) {
+    swatch.addEventListener('change', function() {
+      // Haal de juiste gegevens van de geselecteerde swatch
+      var variantId = this.dataset.variantId;
+      var variantImage = this.dataset.variantImage;
+      var variantAlt = this.dataset.variantAlt;
+      var variantColor = this.dataset.variantColor;
+  
+      // Update de featured image
+      var featuredImage = document.querySelector('#featured-image-' + variantId);
+      if (featuredImage) {
+        featuredImage.src = variantImage;
+        featuredImage.alt = variantAlt;
+      }
+  
+      // Update de media-thumbnails
+      document.querySelectorAll('.product__media-item').forEach(function(thumbnail) {
+        if (thumbnail.dataset.variantColor === variantColor || variantColor === 'all') {
+          thumbnail.querySelector('img').src = variantImage;
+          thumbnail.querySelector('img').alt = variantAlt;
+        }
+      });
+    });
+  });
+  
