af776895f168ed9ec9b629c1c5ef5ae927109562
Daniel de Vos <daniel.devos@wearetriple.com>
2024-12-27 11:38:53 +0100

    applySortAndFilters() {   console.log('applySortAndFilters called'); // Debugging   const queryString = this.buildQueryParams();   console.log('Query string:', queryString);   this.updateURLHash(queryString);   this.renderPage(queryString);   this.updateProductCount();
      // Re-initialize sliders after sorting and filtering
      this.reinitializeSliders();
    }
    
    // Re-initialize all sliders after filtering and sorting
    reinitializeSliders() {
      document.querySelectorAll('.product-card-media-slider').forEach(slider => new Slider(slider));
    }

--- assets/facets.js
+++ assets/facets.js
@@ -286,8 +286,16 @@ class FacetFiltersForm extends HTMLElement {
     this.updateURLHash(queryString);
     this.renderPage(queryString);
     this.updateProductCount();
-  }  
-
+    
+    // Re-initialize sliders after sorting and filtering
+    this.reinitializeSliders();
+  }
+  
+  // Re-initialize all sliders after filtering and sorting
+  reinitializeSliders() {
+    document.querySelectorAll('.product-card-media-slider').forEach(slider => new Slider(slider));
+  }
+  
   clearFilters() {
     // Add radio reset
     this.querySelectorAll('input[type="radio"]').forEach(input => {
