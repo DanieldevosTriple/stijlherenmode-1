5f55895fb2e57b57cb6dff1bac77863f6389c214
Daniel de Vos <daniel.devos@wearetriple.com>
2025-01-05 15:16:06 +0100

    navigation-banner

--- assets/dynamic-header-desktop.js
+++ assets/dynamic-header-desktop.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const sectionHeader = document.querySelector('.section-header');
     const sectionIndexPage = document.querySelector('.index-page');
-    const sectionAnnouncementBar = document.querySelector('.announcement-bar-section'); 
+    const sectionAnnouncementBar = document.querySelector('.navigation-banner'); 
     
     if (!sectionHeader) {
         console.warn('Element with class "section-header" not found. Check if the class is correctly set in HTML.');
